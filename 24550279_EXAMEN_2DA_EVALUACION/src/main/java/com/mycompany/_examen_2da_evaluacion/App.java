80518436e32a91b870631cc05064a998b128ec05
Armandooiou <L24550279@chihuahua2.tecnm.mx>
2024-11-08 17:01:28 -0600

    Add files via upload

---
+++ 24550279_EXAMEN_2DA_EVALUACION/src/main/java/com/mycompany/_examen_2da_evaluacion/App.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._examen_2da_evaluacion;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aaro1
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    
+        var usuarioElije = ("Piedra, papel, o tigera");
+        var computadoraElije = Math.random();
+        if (computadoraElije <0,34)
+            computadoraElije = "piedra";
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
